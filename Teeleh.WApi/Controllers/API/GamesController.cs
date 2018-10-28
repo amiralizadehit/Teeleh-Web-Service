6c9ad1cbc08409e12661362f4d37ee02459e78fe
DESKTOP-I2GHGOP\Amir <amiralizadeh.it@gmail.com>
2018-10-28 03:53:31 +0300

    Game Panel Form Test

--- Teeleh.WApi/Controllers/API/GamesController.cs
+++ Teeleh.WApi/Controllers/API/GamesController.cs
@@ -16,7 +16,18 @@ namespace Teeleh.WApi.Controllers.API
 
         public IHttpActionResult GetGames()
         {
-            var games = db.Games.Include(p => p.SupportedPlatforms).Include(g => g.Genres).Include(a => a.Avatar).ToList();
+            var games = db.Games.Include(p => p.SupportedPlatforms).Include(g => g.Genres).Include(a => a.Avatar).Select(x=>new
+            {
+                x.Name,
+                x.ReleaseDate,
+                x.Genres,
+                x.OnlineCapability,
+                x.Developer,
+                x.Publisher,
+                x.Avatar,
+                x.MetaScore,
+                x.UserScore,
+            });
 
             return Ok(games);
         }
